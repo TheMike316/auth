4a90751db12b4ca3023cd01ae9b0a901a1c2c4a7
mikeh <mike.hornegger@gmail.com>
2018-01-15 17:26:01 +0100

    adding logout functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggendIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCRSk9-bhrqTz-Y0Selg5tgmLTKDVNFo7w',
@@ -14,12 +16,38 @@ class App extends Component {
       storageBucket: 'authentication-57f47.appspot.com',
       messagingSenderId: '241852324413'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggendIn: true });
+      } else {
+        this.setState({ loggendIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggendIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
